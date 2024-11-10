a3efb012a2fdb58be45d2eb4ab749d92ce11d9c0
Vaiditya2207 <vaiditya.t23csai@nst.rishihood.edu.in>
2024-11-10 09:23:21 +0530

    Longitude Latitude mistake

--- controllers/hospitalController.js
+++ controllers/hospitalController.js
@@ -7,12 +7,13 @@ const { findNearestHospitals } = require('../utils/hospitalLocator');
  * @param {Object} res - Express response object.
  */
 const getNearestHospitals = async (req, res) => {
-  let { long: longitude, lat: latitude } = req.body;
-
+  let { longitude, latitude } = req.body;
   longitude = parseFloat(longitude);
   latitude = parseFloat(latitude);
+  console.log(longitude, latitude);
   try {
     const hospitals = await findNearestHospitals(longitude, latitude);
+    console.log(hospitals);
     res.status(200).json({ hospitals });
   } catch (error) {
     res.status(500).json({ error: 'Failed to retrieve nearest hospitals.' });
